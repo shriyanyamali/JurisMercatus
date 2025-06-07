0c47f3eae6e9f16915adf4055cb32f8299b96241
ShriyanYamali <srujanshriyan@gmail.com>
2025-06-06 22:57:11 -0400

    added new cases

--- app/search/page.js
+++ app/search/page.js
@@ -153,7 +153,7 @@ export default function Home() {
   const generateYearOptions = () => {
     const currentYear = new Date().getFullYear();
     return Array.from(
-      { length: currentYear - 2020 },
+      { length: currentYear - 2017 },
       (_, i) => currentYear - i
     );
   };
