78e2533940580578ad59c0551f61a6e14087c6f2
ShriyanYamali <srujanshriyan@gmail.com>
2025-07-13 16:45:07 -0400

    expanded decision year filter to include 2016

--- app/search/page.js
+++ app/search/page.js
@@ -137,7 +137,7 @@ export default function Home() {
   const generateYearOptions = () => {
     const currentYear = new Date().getFullYear();
     return Array.from(
-      { length: currentYear - 2016 },
+      { length: currentYear - 2015 },
       (_, i) => currentYear - i
     );
   };
