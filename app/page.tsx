3930e2051109d61841fef412a81544bf760c891b
ShriyanYamali <srujanshriyan@gmail.com>
2025-07-19 18:39:11 -0400

    added what is verdictr and why use verdictr sections

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Camp from "@/components/Camp";
 import Features from "@/components/Features";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
+import WhatIsVerdictr from '@/components/WhatIsVerdictr';
 
 export const metadata: Metadata = {
   title: 'Verdictr',
@@ -12,6 +13,7 @@ export default function HomePage() {
   return (
     <>
       <Hero />
+      <WhatIsVerdictr />
       <Camp />
       <Guide />
       <Features />
