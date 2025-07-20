2bc9daad13f0b28926b268aa01dc30080f4df6a4
ShriyanYamali <srujanshriyan@gmail.com>
2025-07-20 04:37:46 -0400

    added how to use component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
-import { Metadata } from 'next';
-import WhatIsVerdictr from '@/components/WhatIsVerdictr';
-import Camp from "@/components/Camp";
-import Features from "@/components/Features";
-import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
-import Applications from '@/components/Applications';
-import Benefits from '@/components/Benefits';
+import WhatIsVerdictr from "@/components/WhatIsVerdictr";
+import Benefits from "@/components/Benefits";
+import Applications from "@/components/Applications";
+import HowToUse from "@/components/HowToUse";
 
 export const metadata = {
-  title: 'Home - Verdictr',
-  description: 'Homepage for Verdictr. Made by Shriyan Yamali.',
-}
+  title: "Home - Verdictr",
+  description: "Homepage for Verdictr. Made by Shriyan Yamali.",
+};
 
 export default function HomePage() {
   return (
     <>
       <Hero />
       <WhatIsVerdictr />
+      <HowToUse />
       <Benefits />
       <Applications />
     </>
