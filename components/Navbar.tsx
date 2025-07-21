4ccc10f4cd8b5a209070a89f2cc419bdd3e49a38
ShriyanYamali <srujanshriyan@gmail.com>
2025-07-20 23:29:10 -0400

    changed contact background color on hover

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -86,7 +86,7 @@ const Navbar: React.FC = () => {
             <Link
               href="mailto:yamalishriyan@gmail.com"
               target="_blank"
-              className="
+              className={`
           group
           relative
           inline-flex items-center justify-center
@@ -96,19 +96,21 @@ const Navbar: React.FC = () => {
           text-[1rem] leading-none   
           hover:bg-[#FFFAFA] border hover:border-gray-900 hover:text-gray-900
           transition duration-300 ease-in-out
-        "
+          ${isScrolled ? "hover:bg-[#F2F0EF]" : "hover:bg-[#FFFAFA]"}
+        `}
             >
               Contact
               <Send
-                className="
+                className={`
                 absolute right-2
-                bg-[#FFFAFA] rounded-full p-2
+                rounded-full p-2
                 text-black w-10 h-10
                 transition duration-300 ease-in-out
                 group-hover:bg-gray-900
                 group-hover:text-white
                 overflow-visible
-              "
+                ${isScrolled ? "bg-[#F2F0EF]" : "bg-[#FFFAFA]"}
+              `}
               />
             </Link>
           </div>
@@ -139,7 +141,7 @@ const Navbar: React.FC = () => {
           ${isScrolled ? "bg-[#F2F0EF]" : "bg-[#FFFAFA]"}
         `}
       >
-        <ul className="flex flex-col gap-6 p-6">
+        <ul className="flex flex-col gap-6 px-6 pt-6 pb-8">
           {NAV_LINKS.map((link) => {
             const isActive = pathname === link.href;
             return (
